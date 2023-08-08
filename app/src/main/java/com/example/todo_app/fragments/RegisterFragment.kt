aa98061f9c66d95f61214123695bc033ebaaa61b
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-08-08 09:33:16 +0700

    update user register function

--- app/src/main/java/com/example/todo_app/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/todo_app/fragments/RegisterFragment.kt
@@ -1,4 +1,74 @@
 package com.example.todo_app.fragments
 
-class RegisterFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.todo_app.R
+import com.example.todo_app.databinding.RegisterFragmentBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
+class RegisterFragment : Fragment() {
+
+    private lateinit var navController: NavController
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var binding: RegisterFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = RegisterFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        init(view)
+
+        binding.textViewSignIn.setOnClickListener {
+            navController.navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+
+        binding.nextBtn.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passEt.text.toString()
+            val verifyPass = binding.verifyPassEt.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && verifyPass.isNotEmpty()) {
+                if (pass == verifyPass) {
+
+                    registerUser(email, pass)
+
+                } else {
+                    Toast.makeText(context, "Mật khẩu không khớp", Toast.LENGTH_SHORT).show()
+                }
+            } else
+                Toast.makeText(context, "Email hoặc mật khẩu không được để trống", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun registerUser(email: String, pass: String) {
+        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+            if (it.isSuccessful)
+                navController.navigate(R.id.action_registerFragment_to_homeFragment)
+            else
+                Toast.makeText(context, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    private fun init(view: View) {
+        navController = Navigation.findNavController(view)
+        mAuth = FirebaseAuth.getInstance()
+    }
 }
