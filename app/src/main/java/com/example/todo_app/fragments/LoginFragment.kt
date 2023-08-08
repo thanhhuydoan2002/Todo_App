42f55b21193ac0f46c277f21a387c899adc77f81
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-08-08 11:15:58 +0700

    update user login function

--- app/src/main/java/com/example/todo_app/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/todo_app/fragments/LoginFragment.kt
@@ -1,4 +1,65 @@
-package com.example.todo_app.fragments
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.todo_app.R
+import com.example.todo_app.databinding.LoginFragmentBinding
+import com.google.firebase.auth.FirebaseAuth
 
-class LoginFragment {
+class LoginFragment : Fragment() {
+
+    private lateinit var navController: NavController
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var binding: LoginFragmentBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = LoginFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+
+        binding.textViewSignUp.setOnClickListener {
+            navController.navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.nextBtn.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passEt.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty())
+
+                loginUser(email, pass)
+            else
+                Toast.makeText(context, "Email hoặc mật khẩu không được để trống", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun loginUser(email: String, pass: String) {
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+            if (it.isSuccessful)
+                navController.navigate(R.id.action_loginFragment_to_homeFragment)
+            else
+                Toast.makeText(context, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    private fun init(view: View) {
+        navController = Navigation.findNavController(view)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
 }
